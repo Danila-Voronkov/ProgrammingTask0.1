292f4e426b2c474ce3ec7b526f35be9883935592
danil <danil@LAPTOP-95H61VHK>
2022-09-16 20:35:36 +0700

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WinFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBoxWeight = new System.Windows.Forms.TextBox();
+			this.textBoxRadius = new System.Windows.Forms.TextBox();
+			this.textBoxDensity = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.textBoxWeight);
+			this.panel1.Controls.Add(this.textBoxRadius);
+			this.panel1.Controls.Add(this.textBoxDensity);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(346, 354);
+			this.panel1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.button1.Location = new System.Drawing.Point(0, 242);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(346, 112);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Расчетать толщену";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label4
+			// 
+			this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label4.Location = new System.Drawing.Point(0, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(346, 85);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Расчёт толщены диска, зная его плотность, радиус и массу.";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBoxWeight
+			// 
+			this.textBoxWeight.Location = new System.Drawing.Point(175, 172);
+			this.textBoxWeight.Name = "textBoxWeight";
+			this.textBoxWeight.Size = new System.Drawing.Size(125, 27);
+			this.textBoxWeight.TabIndex = 5;
+			// 
+			// textBoxRadius
+			// 
+			this.textBoxRadius.Location = new System.Drawing.Point(175, 139);
+			this.textBoxRadius.Name = "textBoxRadius";
+			this.textBoxRadius.Size = new System.Drawing.Size(125, 27);
+			this.textBoxRadius.TabIndex = 4;
+			// 
+			// textBoxDensity
+			// 
+			this.textBoxDensity.Location = new System.Drawing.Point(175, 106);
+			this.textBoxDensity.Name = "textBoxDensity";
+			this.textBoxDensity.Size = new System.Drawing.Size(125, 27);
+			this.textBoxDensity.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(33, 175);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(82, 20);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Масса (КГ)";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(33, 142);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(83, 20);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Радиус (M)";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(33, 109);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(131, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Плотность (КГ/M)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(346, 354);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBoxWeight;
+		private System.Windows.Forms.TextBox textBoxRadius;
+		private System.Windows.Forms.TextBox textBoxDensity;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private bool CheckParse(string a)
+		{
+			double s;
+			bool flag;
+			flag = double.TryParse(a, out s);
+			return flag;
+		}
+
+		private double Parse(string a)
+		{
+			double s;
+			double.TryParse(a, out s);
+			return s;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string density = textBoxDensity.Text;
+			if (CheckParse(density) == false || Parse(density) == 0)
+			{
+				MessageBox.Show("Некорректные данные в строке {Плотность}  ");
+				return;
+			}
+			string radius = textBoxRadius.Text;
+			if (CheckParse(radius) == false || Parse(radius) == 0)
+			{
+				MessageBox.Show("Некорректные данные в строке {Радиус}  ");
+				return;
+			}
+			string weight = textBoxWeight.Text;
+			if (CheckParse(weight) == false || Parse(weight) == 0)
+			{
+				MessageBox.Show("Некорректные данные в строке {Масса}  ");
+				return;
+			}
+
+			double width = Parse(weight) / (Math.PI * Math.Pow(Parse(radius), 2) * Parse(density));
+			MessageBox.Show("Толщина (м) равна " +
+				Convert.ToString(width));
+		}
+	}
+}
